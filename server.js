5c489913a04ef12a6a4377ccee9a8f33e2f416c8
Johanna L <johannarleal@gmail.com>
2021-02-11 11:18:34 -0500

    Pseudo code

--- server.js
+++ server.js
@@ -7,11 +7,23 @@ const app = express();
 // setup a port
 const PORT = process.env.PORT || 4500;
 
+// link to array variables that will hold data
 
 // routes
-//app.get("/", (req, ))
+    // set up routes for getting table data
+
+    // set up routes for posting table data
+
 
 // start the server listening for requests
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 })
+
+// set up routes for displaying the HTML pages
+
+// use jQuery to run AJAX calls to GET data from users
+// to the Express server
+
+// use jQuery to run AJAX calls to POST data from users
+// to the Express server
